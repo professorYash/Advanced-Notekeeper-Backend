46afa2575086830b0def3233ed6dd0f046001a3d
Yash86400-Design <knowledge86400@gmail.com>
2024-01-25 10:39:15 +0530

    Removed extra redundant code

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const path = require("path");
+require("dotenv").config();
+require("colors");
+require("path");
 const express = require("express");
 const connectDB = require("./config/db");
 const { errorHandler } = require("./middleware/errorMiddleware");
-const dotenv = require("dotenv").config();
 const port = process.env.PORT || 6000;
-const colors = require("colors");
 
 connectDB();
 
@@ -16,19 +16,6 @@ app.use(errorHandler);
 
 app.use("/api/notes", require("./routes/noteRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
-
-// Serve Frontend
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
-  );
-} else {
-  app.get("/", (req, res) => res.send("Please set to production"));
-}
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
